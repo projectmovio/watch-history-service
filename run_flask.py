cf53ad6506d44ea2e137c7440a2e85ad9d0b44ca
fulder <misad90@gmail.com>
2020-07-18 15:46:27 +0200

    Remove old flask files

--- run_flask.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from subprocess import Popen
-
-os.environ["FLASK_APP"] = "service/server.py"
-flask_ps = Popen(["flask", "run", "--host=0.0.0.0", "--port=8083"])
-flask_ps.communicate()
--- service/dto/movie_dto.py
+++
@@ -1,5 +0,0 @@
-class MovieDto:
-
-    @staticmethod
-    def create(movie):
-        return {"movie_id": movie.movie_id, "watch_date": movie.watch_date}
