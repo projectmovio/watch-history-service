ed52131ac04867ef056923b463fb0fa9d963460f
fulder <misad90@gmail.com>
2020-07-20 22:40:41 +0200

    Add tests for watch_history handler

---
+++ test/unittest/test_watch_history.py
@@ -0,0 +1,78 @@
+import json
+from decimal import Decimal
+from unittest.mock import MagicMock
+
+from api.watch_history import ALLOWED_SORT
+
+TEST_CLIENT_ID = "TEST_CLIENT_ID"
+TEST_JWT = "eyJraWQiOiIxMjMxMjMxMjM9IiwiYWxnIjoiSFMyNTYifQ.eyJjbGllbnRfaWQiOiJURVNUX0NMSUVOVF9JRCJ9.tmBhM3qCJrWJ-bebHXsO9lfmNbF7kYGvMH_qbzNojZQ"
+
+MOCK_KWARGS = {}
+MOCK_ARGS = {}
+MOCK_RETURN = None
+
+
+def mock_func(*args, **kwargs):
+    global MOCK_KWARGS, MOCK_ARGS
+    MOCK_KWARGS = kwargs
+    MOCK_ARGS = args
+    return MOCK_RETURN
+
+
+def test_handler(mocked_watch_history_handler):
+    global MOCK_RETURN
+    MOCK_RETURN = [{"collection_name": "ANIME", "item_id": Decimal(123)}]
+    mocked_watch_history_handler.watch_history_db.get_watch_history = mock_func
+
+    event = {
+        "headers": {
+            "authorization": TEST_JWT
+        }
+    }
+
+    ret = mocked_watch_history_handler.handle(event, None)
+
+    assert MOCK_ARGS == (TEST_CLIENT_ID,)
+    assert MOCK_KWARGS == {"index_name": None, "limit": 100, "start": 1}
+    assert ret == {"body": '[{"collection_name": "ANIME", "item_id": 123}]', "statusCode": 200}
+
+
+def test_handler_invalid_sort(mocked_watch_history_handler):
+    mocked_watch_history_handler.watch_history_db.get_watch_history = mock_func
+
+    event = {
+        "headers": {
+            "authorization": TEST_JWT
+        },
+        "queryStringParameters": {
+            "sort": "INVALID"
+        }
+    }
+
+    ret = mocked_watch_history_handler.handle(event, None)
+
+    assert MOCK_ARGS == (TEST_CLIENT_ID,)
+    assert MOCK_KWARGS == {"index_name": None, "limit": 100, "start": 1}
+    assert ret == {
+        "statusCode": 400,
+        "body": json.dumps({"error": f"Invalid sort specified. Allowed values: {ALLOWED_SORT}"})
+    }
+
+
+def test_handler_sort(mocked_watch_history_handler):
+    mocked_watch_history_handler.watch_history_db.get_watch_history = mock_func
+
+    event = {
+        "headers": {
+            "authorization": TEST_JWT
+        },
+        "queryStringParameters": {
+            "sort": "date_watched"
+        }
+    }
+
+    ret = mocked_watch_history_handler.handle(event, None)
+
+    assert MOCK_ARGS == (TEST_CLIENT_ID,)
+    assert MOCK_KWARGS == {'index_name': 'date_watched', 'limit': 100, 'start': 1}
+    assert ret == {'body': '[{"collection_name": "ANIME", "item_id": 123}]', 'statusCode': 200}
