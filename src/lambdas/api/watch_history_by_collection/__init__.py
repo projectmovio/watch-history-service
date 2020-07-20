bcfe386143590b7ec1ee713578a01fcde5188f90
fulder <misad90@gmail.com>
2020-07-20 22:45:40 +0200

    Add lambda to get watch history by collection

---
+++ src/lambdas/api/watch_history_by_collection/__init__.py
@@ -0,0 +1,50 @@
+import json
+
+import decimal_encoder
+import logger
+import jwt_utils
+import watch_history_db
+
+log = logger.get_logger("watch_history")
+
+ALLOWED_SORT = ["rating", "date_watched", "state"]
+
+
+def handle(event, context):
+    auth_header = event["headers"]["authorization"]
+    client_id = jwt_utils.get_client_id(auth_header)
+
+    collection_name = event["pathParameters"].get("collection_name")
+
+    sort = None
+    query_params = event.get("queryStringParameters")
+    if query_params:
+        sort = query_params.get("sort")
+
+    if sort and sort not in ALLOWED_SORT:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": f"Invalid sort specified. Allowed values: {ALLOWED_SORT}"})
+        }
+
+    limit = 100
+    start = 1
+
+    query_params = event.get("queryStringParameters")
+    if query_params and "limit" in query_params:
+        limit = query_params.get("limit")
+    if query_params and "start" in query_params:
+        start = query_params.get("start")
+
+    try:
+        limit = int(limit)
+    except ValueError:
+        return {"statusCode": 400, "body": json.dumps({"message": "Invalid limit type"})}
+    try:
+        start = int(start)
+    except ValueError:
+        return {"statusCode": 400, "body": json.dumps({"message": "Invalid start type"})}
+
+    watch_history = watch_history_db.get_watch_history(client_id, collection_name=collection_name, index_name=sort, limit=limit, start=start)
+
+    return {"statusCode": 200, "body": json.dumps(watch_history, cls=decimal_encoder.DecimalEncoder)}
