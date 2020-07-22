b4dc6f5480b1e21147a4e77a9891ec0efedb5e30
fulder <misad90@gmail.com>
2020-07-22 13:47:02 +0200

    Start adding test for post_item

--- test/apitest/conftest.py
+++ test/apitest/conftest.py
@@ -1,6 +1,7 @@
 import os
 
 API_URL = os.getenv("API_URL")
+ANIME_API_URL = os.getenv("ANIME_API_URL")
 BASE_HEADERS = {
     "Authorization": os.getenv("TOKEN")
 }
--- test/apitest/test_watch_history.py
+++ test/apitest/test_watch_history.py
@@ -2,17 +2,30 @@ import time
 
 import requests
 
-from apitest.conftest import API_URL, BASE_HEADERS
+from apitest.conftest import API_URL, BASE_HEADERS, ANIME_API_URL
 
 
-def test_get_watch_history_invalid_auth():
-    res = requests.get(f"{API_URL}/watch-history", headers={"Authorization": "Invalid"})
+# def test_get_watch_history_invalid_auth():
+#     res = requests.get(f"{API_URL}/watch-history", headers={"Authorization": "Invalid"})
+#
+#     assert res.status_code == 401
+#     time.sleep(1)
+#
+#
+# def test_get_invalid_collection_item():
+#     res = requests.get(f"{API_URL}/watch-history/collection/invalid/123", headers=BASE_HEADERS)
+#
+#     assert res.status_code == 400
+#     assert res.json() == {'message': "Invalid collection name, allowed values: ['anime', 'show', 'movie']"}
+#     time.sleep(1)
 
-    assert res.status_code == 401
 
+def test_post_anime_item():
+    res = requests.post(f"{ANIME_API_URL}/anime?mal_id=21", headers=BASE_HEADERS)
+    anime_id = res.json()["anime_id"]
 
-def test_post_item():
-    res = requests.get(f"{API_URL}/watch-history/collection/invalid/123", headers=BASE_HEADERS)
+    res = requests.post(f"{API_URL}/watch-history/collection/anime/{anime_id}", headers=BASE_HEADERS)
 
     assert res.status_code == 400
     assert res.json() == {'message': "Invalid collection name, allowed values: ['anime', 'show', 'movie']"}
+    time.sleep(1)
