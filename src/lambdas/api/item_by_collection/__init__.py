18c7f842a607742743448233ebe6fa1d64597120
fulder <misad90@gmail.com>
2020-07-22 11:43:11 +0200

    Refactor item_by_collection in helper methods per method

--- src/lambdas/api/item_by_collection/__init__.py
+++ src/lambdas/api/item_by_collection/__init__.py
@@ -22,19 +22,31 @@ def handle(event, context):
     item_id = event["pathParameters"].get("item_id")
 
     if method == "GET":
-        try:
-            ret = watch_history_db.get_item(client_id, collection_name, item_id)
-            return {"statusCode": 200, "body": json.dumps(ret)}
-        except watch_history_db.NotFoundError:
-            return {"statusCode": 404}
+        return _get_item(client_id, collection_name, item_id)
     elif method == "POST":
         body = event.get("body")
-        try:
-            schema.validate_schema(POST_SCHEMA_PATH, body)
-        except schema.ValidationException as e:
-            return {"statusCode": 400, "body": json.dumps({"message": "Invalid post schema", "error": str(e)})}
-        watch_history_db.update_item(client_id, collection_name, item_id, body)
-        return {"statusCode": 204}
+        return _post_item(client_id, collection_name, item_id, body)
     elif method == "DELETE":
-        watch_history_db.delete_item(client_id, collection_name, item_id)
-        return {"statusCode": 204}
+        return _delete_item(client_id, collection_name, item_id)
+
+
+def _get_item(client_id, collection_name, item_id):
+    try:
+        ret = watch_history_db.get_item(client_id, collection_name, item_id)
+        return {"statusCode": 200, "body": json.dumps(ret)}
+    except watch_history_db.NotFoundError:
+        return {"statusCode": 404}
+
+
+def _post_item(client_id, collection_name, item_id, body):
+    try:
+        schema.validate_schema(POST_SCHEMA_PATH, body)
+    except schema.ValidationException as e:
+        return {"statusCode": 400, "body": json.dumps({"message": "Invalid post schema", "error": str(e)})}
+    watch_history_db.update_item(client_id, collection_name, item_id, body)
+    return {"statusCode": 204}
+
+
+def _delete_item(client_id, collection_name, item_id):
+    watch_history_db.delete_item(client_id, collection_name, item_id)
+    return {"statusCode": 204}
