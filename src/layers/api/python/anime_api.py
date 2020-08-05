da34f5260328f92322ec7ecdbf6291dfd119b0b0
fulder <misad90@gmail.com>
2020-08-05 13:29:31 +0200

    Update route path for get_animes

--- src/layers/api/python/anime_api.py
+++ src/layers/api/python/anime_api.py
@@ -23,7 +23,7 @@ def post_anime(mal_id, token):
 
 def get_animes(ids, token):
     ids = ",".join(ids)
-    res = requests.get(f"{ANIME_API_URL}/anime/{ids}", headers={"Authorization": token})
+    res = requests.get(f"{ANIME_API_URL}/animes/{ids}", headers={"Authorization": token})
     if res.status_code != 200:
         raise HttpError(f"Invalid response: {res.status_code}")
 
