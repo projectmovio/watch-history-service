be9b617c6eb2f61ebe0fc431c3d2c5baae140e76
fulder <misad90@gmail.com>
2021-01-01 19:45:53 +0100

    Remove get_animes tests

--- test/unittest/test_anime_api.py
+++ test/unittest/test_anime_api.py
@@ -25,25 +25,3 @@ def test_post_anime_invalid_code(mocked_get, mocked_anime_api):
 
     with pytest.raises(api_errors.HttpError):
         mocked_anime_api.get_anime("123", "TEST_TOKEN")
-
-
-@patch("anime_api.requests.get")
-def test_get_animes(mocked_get, mocked_anime_api):
-    m = MagicMock()
-    m.status_code = 200
-    m.json.return_value = {"anime_id": "123"}
-    mocked_get.return_value = m
-
-    ret = mocked_anime_api.get_animes("123,456", "TEST_TOKEN")
-
-    assert ret == {'anime_id': '123'}
-
-
-@patch("anime_api.requests.get")
-def test_get_animes_invalid_code(mocked_get, mocked_anime_api):
-    m = MagicMock()
-    m.status_code = 404
-    mocked_get.return_value = m
-
-    with pytest.raises(api_errors.HttpError):
-        mocked_anime_api.get_animes("123", "TEST_TOKEN")
