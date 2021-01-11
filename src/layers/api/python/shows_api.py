f9bf827b11bd514cce0d7629f0de009c72a6dc0c
fulder <misad90@gmail.com>
2021-01-11 01:08:45 +0100

    Change show to shows in api path

--- src/layers/api/python/shows_api.py
+++ src/layers/api/python/shows_api.py
@@ -7,7 +7,7 @@ SHOWS_API_URL = os.getenv("SHOWS_API_URL")
 
 
 def get_show(show_id, token):
-    res = requests.get(f"{SHOWS_API_URL}/show/{show_id}", headers={"Authorization": token})
+    res = requests.get(f"{SHOWS_API_URL}/shows/{show_id}", headers={"Authorization": token})
     if res.status_code != 200:
         raise api_errors.HttpError("Invalid response in get_show", res.status_code)
 
