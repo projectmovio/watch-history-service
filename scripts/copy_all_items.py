4380a24847bf3cd3d2753795e6a485813d3b9ad4
fulder <misad90@gmail.com>
2021-01-17 16:26:05 +0100

    Prepare script for tables with hardcoded names

--- scripts/copy_all_items.py
+++ scripts/copy_all_items.py
@@ -5,12 +5,10 @@ client = boto3.client("dynamodb")
 resource = boto3.resource("dynamodb")
 
 OLD_ITEMS_TABLE_NAME = "watch-history-watchhistoryC6E1702F-9FP7GUOWK92O"
-#NEW_ITEMS_TABLE = resource.Table("watch-history-items")
-NEW_ITEMS_TABLE = resource.Table("watch-history-watchhistoryC6E1702F-1PO8A1UNXFW1N")
+NEW_ITEMS_TABLE = resource.Table("watch-history-items")
 
 OLD_EPISODES_TABLE_NAME = "watch-history-episodesDCA28D60-61TCY1BLHGRJ"
-#NEW_EPISODES_TABLE = resource.Table("watch-history-episodes")
-NEW_EPISODES_TABLE = resource.Table("watch-history-episodesDCA28D60-1WD5J6YDTZY98")
+NEW_EPISODES_TABLE = resource.Table("watch-history-episodes")
 
 
 paginator = client.get_paginator('scan')
