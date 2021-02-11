5ae730018201624fe927090e8feabe22e595203c
fulder <misad90@gmail.com>
2021-02-11 23:00:42 +0100

    Add back get_anime tests

--- test/unittest/test_anime_api.py
+++ test/unittest/test_anime_api.py
@@ -5,6 +5,28 @@ import pytest
 import api_errors
 
 
+@patch("anime_api.requests.get")
+def test_get_anime(mocked_get, mocked_anime_api):
+    m = MagicMock()
+    m.status_code = 200
+    m.json.return_value = {"anime_id": "123"}
+    mocked_get.return_value = m
+
+    ret = mocked_anime_api.get_anime("123", "TEST_TOKEN")
+
+    assert ret == {"anime_id": "123"}
+
+
+@patch("anime_api.requests.get")
+def test_post_anime_invalid_code(mocked_get, mocked_anime_api):
+    m = MagicMock()
+    m.status_code = 404
+    mocked_get.return_value = m
+
+    with pytest.raises(api_errors.HttpError):
+        mocked_anime_api.get_anime("123", "TEST_TOKEN")
+
+
 @patch("anime_api.requests.post")
 def test_post_anime(mocked_post, mocked_anime_api):
     m = MagicMock()
