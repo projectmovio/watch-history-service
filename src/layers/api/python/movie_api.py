ea7674544791645ff03a7a25c4784d2eaf56c81b
fulder <misad90@gmail.com>
2021-02-11 22:45:03 +0100

    Add back get functions to apis

--- src/layers/api/python/movie_api.py
+++ src/layers/api/python/movie_api.py
@@ -6,6 +6,14 @@ import api_errors
 MOVIE_API_URL = os.getenv("MOVIE_API_URL")
 
 
+def get_movie(movie_id, token):
+    res = requests.get(f"{MOVIE_API_URL}/movies/{movie_id}", headers={"Authorization": token})
+    if res.status_code != 200:
+        raise api_errors.HttpError("Invalid response in get_show", res.status_code)
+
+    return res.json()
+
+
 def post_movie(body, token):
     res = requests.post(f"{MOVIE_API_URL}/movies", headers={"Authorization": token}, json=body)
     if res.status_code != 200:
--- src/layers/api/python/shows_api.py
+++ src/layers/api/python/shows_api.py
@@ -6,6 +6,14 @@ import api_errors
 SHOWS_API_URL = os.getenv("SHOWS_API_URL")
 
 
+def get_show(show_id, token):
+    res = requests.get(f"{SHOWS_API_URL}/shows/{show_id}", headers={"Authorization": token})
+    if res.status_code != 200:
+        raise api_errors.HttpError("Invalid response in get_show", res.status_code)
+
+    return res.json()
+
+
 def post_show(body, token):
     res = requests.get(f"{SHOWS_API_URL}/shows", headers={"Authorization": token}, json=body)
     if res.status_code != 200:
